4a0d9009bfbb5cc7643f8ca899b20711388d6b45
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-10-01 14:08:34 -0500

    awqerg

---
+++ Models/Survey.cs
@@ -0,0 +1,10 @@
+namespace Survey.Models
+{
+    public class SurveyResults
+    {
+        public string Name {get;set;}
+        public string Dojo {get;set;}
+        public string Language {get;set;}
+        public string Comments {get;set;}
+    }
+}
